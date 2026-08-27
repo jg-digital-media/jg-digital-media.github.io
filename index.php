ef016cad4890c40065a47fd14ffc7a8988f4f275
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-08-27 11:56:03 +0100

    update copy

--- index.php
+++ index.php
@@ -34,11 +34,11 @@
 
     <header>
 
-        <h1> Welcome to..... </h1>
+        <h1> Welcome to... </h1>
+        
+        <h2><span class="" id="jonniegrieve">Jonnie Grieve</span> <span class="" id="digitalmedia">Digital Media</span></h2>
 
         <img src="_assets/media/img/jg-logo-circle.png" alt="Welcome to Jonnie Grieve Digital Media" title="Welcome to Jonnie Grieve Digital Media" />
-
-        <h2><span class="" id="jonniegrieve">Jonnie Grieve</span><br /><span class="" id="digitalmedia">Digital Media</span></h2>
         
     </header>
 
@@ -56,9 +56,9 @@
                 <source src="_assets/media/video/jonnie-grieve-digital-media.mp4"  type="video/mp4" alt="Please see our promotional video" title="Please see our promotional video" />
             </video>
 
-            <p>JGDM is the independent website and multimedia design agency based in the North East of England.</p>
+            <p>Jonnie Grieve Digital Media (JGDM) is the independent website and multimedia design agency based in the North East of England.</p>
 
-            <p>At Jonnie Grieve Digital Media, I specialise in designing front-end websites across a range of industries,  helping you get your new business the web presence it needs.</p>
+            <p>At JGDM, I specialise in designing front-end websites across a range of industries,  helping you get your new business the web presence it needs.</p>
 
             <p>My core business includes:</p>
 
@@ -72,24 +72,27 @@
 
             </ul>   
             
-            <!-- TODO: --> <p id="worked_with">I've worked with a wide range of clients including [client name], [client name] and [client name]. </p>
-            
-            <!--<p>Random projects display  4 random projects  Visit more on my portfolio </p>-->
+            <p id="worked_with"> </p>            
                    
             <h2 class="lead" id="who">Who am I, and what do I do?</h2>
 
-            <p>Jonnie Grieve Digital Media was created in 2012 by its founder and lead designer, Jonnie Grieve, with the intention of providing professional, cost-effective web design across the North East, the UK and beyond. Additional services are also available, including Photo Editing, Image Processing and Multimedia Design, all at prices that allow start up businesses to receive a <strong>high</strong> level of quality for a <strong>low</strong> initial outlay.</p>            
+            <p><strong>Jonnie Grieve Digital Media</strong> was created in <strong>2012</strong> by its founder and lead designer, Jonnie Grieve, with the intention of providing professional, cost-effective web design across the North East, the UK and beyond.</p>
+            
+            <p>Additional services are also available, including Photo Editing, Image Processing and Multimedia Design, all at prices that allow start up businesses to receive a <strong>high</strong> level of quality for a <strong>low</strong> initial outlay.</p>            
 
             <p>Specialist areas include <strong>jQuery</strong>, <strong>PHP</strong> and <strong>WordPress</strong>, to solve digital issues, and using industry tools including Google Web Designer, Dreamweaver, Visual Studio Code, Atom and others. This, combined with training in <strong>HTML</strong> and <strong>CSS3</strong> makes this comprehensive service one that is experienced in meeting your digital media needs.                 
                 
             <h2 class="lead" id="projects">Just a few of my projects</h2>
 
             <div class="div_projectcontainer" id ="projectContainer">
+                
                 <a href="https://www.autoc.co.uk" target="blank"> <img src="https://www.jonniegrieve.co.uk/assets/lists/project_list/assets/img/list/autoc.png" class="project_image" alt="Project" title="Project" /> </a>
                 <a href="https://www.kevinmangles-magician.com" target="blank"> <img src="https://www.jonniegrieve.co.uk/assets/lists/project_list/assets/img/list/mangles.png" class="project_image" alt="Project" title="Project" /> </a><br />
                 <a href="https://dyspraxia.jonniegrieve.co.uk" target="blank"> <img src="https://www.jonniegrieve.co.uk/assets/lists/project_list/assets/img/list/dyspraxia.png" class="project_image" alt="Project" title="Project" /> </a>
                 <a href="https://www.creativeio.co.uk" target="blank"> <img src="https://www.jonniegrieve.co.uk/assets/lists/project_list/assets/img/list/creativeio.png" class="project_image" alt="Project" title="Project" /> </a>
+
             </div>  
+            
             <a href="#" id="refresh_projects">Refresh Projects</a>            
             
             <p class="more_projects">Find more of my projects on my portfolio</p>
@@ -98,15 +101,19 @@
 
             <h2 class="lead" id="why">Why Choose Jonnie Grieve Digital Media?</h2>  
 
-            <p>In today's digital market, all companies require an online presence to advertise their brand to the world. As the Internet continues to grow we become more and more reliant on it to buy our goods, communicate with each other and reach out to our customers.</p>            
+            <p>When I first created Jonnie Grieve Digital Media, the digital revolution was just beginning. Now the landscape has changed. All companies require an online presence to advertise their brand to the world. </p>
+            
+            <p>We have become so reliant on our devices, small or large, to buy our goods, to communicate with each other and reach out to our customers.</p>            
 
-            <p>JGDM is a personal and professional service with over a decade of prior academic experience in the field of design. We work flexible hours on a job by job basis, with no project too small. We offer free advice on the best possible designs for your business. You will pay nothing more than the agreed terms, so there are never nasty surprises. We are committed to achieving the results agreed upon when you choose us to design your website. </p>
+            <p>JGDM is a personal and professional service with over a decade of prior academic experience in the field of design. I work flexible hours on a job-by job-basis, and no project is too small.</p>
+            
+            <p>I offer free advice on the best possible designs for your business. You will pay nothing more than the agreed terms, so there are never nasty surprises. We are committed to achieving the results agreed upon when you choose us to design your website.</p>
 
-            <p>We are customer focused and care deeply about the products and services we create. We are meticulous and steadfast in our approach. Web Design is a creative process and one that is never truly finished. We are always looking for ways to improve our performance.</p>
+            <p>JGDM is a customer-focused business and cares deeply about the products and services we create. Web Design is a creative process and one that is never truly finished. As the lead designer and sole practitioner of the business, I am meticulous and steadfast in my approach, and I am always looking for ways to improve.</p>
 
             <h2 class="lead" id="more">Do you want to know more?</h2>
                     
-            <p>Get in touch for a no obligation chat if you feel that we can help or please see our promotional video below. We look forward to hearing from you.</p>
+            <p>Get in touch for a no-obligation chat if you feel that we can help. And I look forward very much to hearing from you.</p>
 
             <!--<video controls poster="_assets/media/video/video-poster.jpg">
                 <source src="media/video/jonnie-grieve-digital-media.mp4"  type="video/mp4" alt="Please see our promotional video" title="Please see our promotional video" />
